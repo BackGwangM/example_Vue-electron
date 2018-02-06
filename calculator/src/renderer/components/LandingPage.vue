e47570ee188e3df39403dab1783136abccfb9f15
back_gwang_min <back5373122@gmail.com>
2018-02-06 10:25:40 +0900

    calculator setup

---
+++ calculator/src/renderer/components/LandingPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <NumberBtn></NumberBtn>
+</template>
+<script>
+import NumberBtn from './LandingPage/number_btn'
+export default {
+  components:{
+    NumberBtn
+  }
+  
+}
+</script>
+<style scoped>
+
+</style>
---
+++ calculator/src/renderer/components/LandingPage/number_btn.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="number_btn">
+      <div class="btn symbol">
+          <span>Clear</span>
+      </div>
+      <div class="btn symbol">
+          <span>+/-</span>
+      </div>
+      <div class="btn symbol">
+          <span>%</span>
+      </div>
+      <div class="btn calc symbol">
+          <span>/</span>
+      </div>
+      <div class="btn">
+          <span>7</span>
+      </div>
+      <div class="btn">
+          <span>8</span>
+      </div>
+      <div class="btn">
+          <span>9</span>
+      </div>
+      <div class="btn calc symbol">
+          <span>×</span>
+      </div>
+      <div class="btn">
+          <span>4</span>
+      </div>
+      <div class="btn">
+          <span>5</span>
+      </div>
+      <div class="btn">
+          <span>6</span>
+      </div>
+      <div class="btn calc symbol">
+          <span>-</span>
+      </div>
+      <div class="btn">
+          <span>1</span>
+      </div>
+      <div class="btn">
+          <span>2</span>
+      </div>
+      <div class="btn">
+          <span>3</span>
+      </div>
+      <div class="btn calc symbol">
+          <span>+</span>
+      </div>
+      <div class="btn">
+          <span>00</span>
+      </div>
+      <div class="btn">
+          <span>0</span>
+      </div>
+      <div class="btn">
+          <span>.</span>
+      </div>
+      <div class="btn">
+          <div id="result">=</div>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+    #number_btn{
+        position: absolute;
+        bottom: 0;
+    }
+</style>
+
